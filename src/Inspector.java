4a61a4292c39bc17728a47ef7675c92fd9d93da8
ttdjohns <32712899+ttdjohns@users.noreply.github.com>
2019-03-01 21:28:06 -0700

    Declaring, super and interface classes delt with

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,7 +1,35 @@
+import java.lang.reflect.*;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Inspector {
+	List<Class> toInspect = new LinkedList<Class>();
+	List<Class> alreadyInspected = new LinkedList<Class>();
 
 	public void inspect(Object obj, boolean recursive) {
+		String str = "";
+		Class classObj = obj.getClass();
+		alreadyInspected.add(classObj);
 		
+		str += "Declaring Class: " + classObj.getName() + "\n";
+		
+		Class superCl = classObj.getSuperclass();
+		if (superCl != null) {
+			str += "Superclass: " + superCl.getName() + "\n";
+		}
+		Class[] interfaceCl = classObj.getInterfaces();
+		for (int i = 0; i < interfaceCl.length; i++) {
+			str += "Interface: " + interfaceCl[i].getName() + "\n";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println(str);
 	}
 }
